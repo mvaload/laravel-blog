4421d8e52c9abaaaa2efea09fc276ba95c445413
mvaload <mvaload@gmail.com>
2019-09-03 17:17:13 +0300

    handler moved about

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', 'PageController@about');
 
 Route::get('/articles', function () {
     return view('articles');
