dfabbfd11d9bbe614cbac2a594108b63d734fb9f
mvaload <mvaload@gmail.com>
2019-09-04 15:27:58 +0300

    added new controller

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::paginate();
+        return view('article.index', compact('articles'));
+    }
+}
