272537e47b303b7c8a65fe98b01d3860e203de2d
mvaload <mvaload@gmail.com>
2019-09-05 10:12:32 +0300

    added new tamplates

---
+++ resources/views/article/create.blade.php
@@ -0,0 +1,8 @@
+ @extends('layouts.app')
+
+@section('content')
+    {{ Form::model($article, ['url' => route('articles.store')]) }}
+        @include('article.form')
+        {{ Form::submit('Сохранить') }}
+    {{ Form::close() }}
+@endsection
