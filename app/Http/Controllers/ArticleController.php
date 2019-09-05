729414dc0be676b551bebb82147a30e82e02bb73
mvaload <mvaload@gmail.com>
2019-09-05 10:10:02 +0300

    added new action

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,7 +9,7 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = Article::paginate();
+        $articles = Article::paginate(5);
         return view('article.index', compact('articles'));
     }
 
@@ -18,4 +18,31 @@ class ArticleController extends Controller
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
     }
+        // Вывод формы
+    public function create()
+    {
+        // Передаем в шаблон вновь созданный объект. Он нужен для вывода формы через Form::model
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    // Здесь нам понадобится объект запроса, для извлечения данных
+    public function store(Request $request)
+    {
+        // Проверка введенных данных
+        // Если будут ошибки, то возникнет исключение
+        $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:1000',
+        ]);
+
+        $article = new Article();
+        // Заполнение статьи данными из формы
+        $article->fill($request->all());
+        // При ошибках сохранения возникнет исключение
+        $article->save();
+
+        // Редирект на указанный маршрут с добавлением флеш сообщения
+        return redirect()->route('articles.index')->with('success', 'Article created successfully');
+      }
 }
